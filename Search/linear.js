d065ea288b675deb07def60521efe400b34a653a
sazidahmed <sazidahmed.official@gmail.com>
2022-08-31 20:36:36 +0600

    linear search added

---
+++ Search/linear.js
@@ -0,0 +1,10 @@
+function linear(arr, val){
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] == val) return i;
+    }
+    return -1;
+}
+
+var result = linear([1, 4, 2, 5], 4)
+
+console.log(result)

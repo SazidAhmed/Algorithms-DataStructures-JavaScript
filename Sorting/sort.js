dd726dbd5b0b0391bfebbdbc64c919326d7ce0e2
sazidahmed <sazidahmed.official@gmail.com>
2022-09-02 20:12:22 +0600

    working on soring

--- Sorting/sort.js
+++ Sorting/sort.js
@@ -2,6 +2,15 @@ function numberCompare(num1, num2){
     return num1 - num2;
 }
 
-const arr = [6,4,16,12]
-const result = arr.sort(numberCompare)
-console.log(result)
+function compareByLen(str1, str2){
+    return str2.length - str1.length;
+}
+
+const arr1 = [6,4,16,12]
+const arr2 = ['apple', 'dog', 'car', 'ball']
+
+const result1 = arr1.sort(numberCompare)
+console.log(result1)
+
+const result2 = arr2.sort(compareByLen)
+console.log(result2)
